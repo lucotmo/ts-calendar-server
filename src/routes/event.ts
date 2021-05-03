453a979a2d769653b9ce059c3920f6e3964692a3
lucotmo <lucotmo@gmail.com>
2021-05-03 15:37:34 -0500

    feat: validate fields events

--- src/routes/event.ts
+++ src/routes/event.ts
@@ -3,7 +3,10 @@
     /api/events
 */
 import { Router } from "express";
-
+import { check } from "express-validator";
+import { isDate } from "../helpers/isDate";
+import { validateFields } from "../middlewares/validate-fields";
+import { validateJWT } from "../middlewares/validate-jwt";
 import {
 	createEvent,
 	updateEvent,
@@ -13,14 +16,35 @@ import {
 
 const router = Router();
 
+// Todas tienes que pasar por la validación del JWT
+router.use(validateJWT);
+
 // Obtener eventos
 router.get("/", getEvents);
 
 // Crear un nuevo evento
-router.post("/", createEvent);
+router.post(
+	"/",
+	[
+		check("title", "El titulo es obligatorio").not().isEmpty(),
+		check("start", "Fecha de inicio es obligatoria").custom(isDate),
+		check("end", "Fecha de finalización es obligatoria").custom(isDate),
+		validateFields,
+	],
+	createEvent
+);
 
 // Actualizar Evento
-router.put("/:id", updateEvent);
+router.put(
+	"/:id",
+	[
+		check("title", "El titulo es obligatorio").not().isEmpty(),
+		check("start", "Fecha de inicio es obligatoria").custom(isDate),
+		check("end", "Fecha de finalización es obligatoria").custom(isDate),
+		validateFields,
+	],
+	updateEvent
+);
 
 // Borrar evento
 router.delete("/:id", deleteEvent);
