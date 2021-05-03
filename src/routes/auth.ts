f9bbab3eaa312dd1e8fd95efe1a6eba98235e47d
lucotmo <lucotmo@gmail.com>
2021-05-03 15:34:11 -0500

    feat: validate fields auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,14 +3,39 @@
     host + /api/auth
 */
 import { Router } from "express";
+import { check } from "express-validator";
+import { validateFields } from "../middlewares/validate-fields";
+import { validateJWT } from "../middlewares/validate-jwt";
 import { createUser, loginUser, renewToken } from "../controllers/auth";
 
 const router = Router();
 
-router.post("/new", createUser);
+router.post(
+	"/new",
+	[
+		// middlewares
+		check("name", "El nombre es obligatorio").not().isEmpty(),
+		check("email", "El email es obligatorio").isEmail(),
+		check("password", "El password debe de ser de 6 caracteres").isLength({
+			min: 6,
+		}),
+		validateFields,
+	],
+	createUser
+);
 
-router.post("/", loginUser);
+router.post(
+	"/",
+	[
+		check("email", "El email es obligatorio").isEmail(),
+		check("password", "El password debe de ser de 6 caracteres").isLength({
+			min: 6,
+		}),
+		validateFields,
+	],
+	loginUser
+);
 
-router.get("/renew", renewToken);
+router.get("/renew", validateJWT, renewToken);
 
 module.exports = router;
